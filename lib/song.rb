c7fda3660363ea421feaabac8c381b6b6b64a0c9
Adam Schonely <aschonely@gmail.com>
2019-08-10 02:01:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,55 @@
 class Song 
- attr_accessor :name, :artist_name, :genre  
- 
- @@count = 0 
- @@artists = []
- @@genres = []
- 
- def initialize(name,artist, genre)
-   @name = name
-   @artist = artist
-   @genre = genre
-   @@count += 1 
-   @@genres << genre
-   @@artist << artist
-  end 
-  
+attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
   def self.count
     @@count
   end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
+
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artists_count[artist]
-        artists_count += 1 
+      if artist_count[artist]
+        artist_count[artist] += 1 
       else
-        artists_count = 1
-      end  
+        artist_count[artist] = 1
       end
-      artist_count
     end
-    
+    artist_count
+  end
+
   def self.genres
     @@genres.uniq
   end
-  
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1 
+        genre_count[genre] += 1 
       else
-        genre_count = 1
-      end  
+        genre_count[genre] = 1
       end
-      genre_count
+    end
+    genre_count
   end
-end  
+end
     
     
     
