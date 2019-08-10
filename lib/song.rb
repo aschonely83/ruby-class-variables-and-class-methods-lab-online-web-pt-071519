4560c9154bc7c1da5e2744ce1e51b30268f31b6d
Adam Schonely <aschonely@gmail.com>
2019-08-10 01:41:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,6 @@
 class Song 
  attr_accessor :name, :artist_name, :genre  
  
- @@all = []
  
-  def self.create
-   song = self.new
-   song.save
-   song
-  end 
- 
-  def self.new_by_name(name)
-   new_song = self.name
-   new_song.name = name
-   new_song
-  end
-  
-  def self.create_by_name(name)
-    new_song = self.create
-    new_song.name = name
-    new_song
-  end
-  
-  def self.find_by_name(name)
-    self.find_all { |song| song.name == name }
-  end  
-  
-  
-  
-  def self.all
-    @@all 
-  end
-  
-  def save
-    self.class.all << self
-  end  
   
 end  
